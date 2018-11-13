181495a42b7ef58991fbad83594a827b5999f666
Anway <anwaysomani@gmail.com>
2018-11-13 16:28:47 +0530

    Added General File-1
    ScrapInfo for general information referring

---
+++ Goldbin/app/src/main/java/initial/project/com/goldbin/ScrapInfo.java
@@ -0,0 +1,59 @@
+package initial.project.com.goldbin;
+
+public class ScrapInfo {
+
+    private String title, unitPrice, scrapCategory, quantity, totalQuantity;
+
+    public Scrap(String title, String unitPrice, String scrapCategory, String quantity, String totalQuantity) {
+        this.title = title;
+        this.unitPrice = unitPrice;
+        this.scrapCategory = scrapCategory;
+        this.quantity = quantity;
+        this.totalQuantity = totalQuantity;
+    }
+
+    //Referring title
+    public String getTitle {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    //Referring Unit Price
+    public String getUnitPrice {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(String unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    //Referring Scrap Category
+    public String getScrapCategory {
+        return scrapCategory;
+    }
+
+    public void setScrapCategory(String scrapCategory) {
+        this.scrapCategory = scrapCategory;
+    }
+
+    //Referring Quantity Amounted
+    public String getQuantity {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+    //Referring Total Units
+    public String getTotalQuantity {
+        return totalQuantity;
+    }
+
+    public void setTotalQuantity(String totalQuantity) {
+        this.totalQuantity = totalQuantity;
+    }
+}
