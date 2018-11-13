44dcd52b6f2c11490eaf788c136e47eaebc1df09
Anway <anwaysomani@gmail.com>
2018-11-13 11:38:38 +0530

    Added initial project
    Initialized Project

---
+++ Goldbin/app/src/main/java/initial/project/com/goldbin/MainActivity.java
@@ -0,0 +1,13 @@
+package initial.project.com.goldbin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
